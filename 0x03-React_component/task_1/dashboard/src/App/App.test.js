9281e690626ce9eac7b279a48babb9ea748deee5
moreni26 <ayomide2605@gmail.com>
2024-05-16 20:05:05 +0100

    correction

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -7,34 +7,49 @@ describe("<App />", () => {
     const wrapper = shallow(<App />);
   });
 
-  it("contain Notifications component", () => {
+  it("contains Notifications component", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("Notifications")).toHaveLength(1);
   });
 
-  it("contain Header component", () => {
+  it("contains Header component", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("Header")).toHaveLength(1);
   });
 
-  it("contain Login component", () => {
+  it("contains Login component", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("Login")).toHaveLength(1);
   });
 
-  it("contain Footer component", () => {
+  it("contains Footer component", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("Footer")).toHaveLength(1);
   });
 
-  it("CourseList", () => {
+  it("does not contain CourseList when not logged in", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find("CourseList")).toHaveLength(0);
   });
 
-  it("isLoggedIn true", () => {
+  it("contains CourseList and not Login when isLoggedIn is true", () => {
     const wrapper = shallow(<App isLoggedIn />);
     expect(wrapper.find("Login")).toHaveLength(0);
     expect(wrapper.find("CourseList")).toHaveLength(1);
+  });
+
+  it("calls logOut and shows alert when Ctrl+H is pressed", () => {
+    const logOutMock = jest.fn();
+    const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {});
+
+    const wrapper = shallow(<App logOut={logOutMock} />);
+
+    const event = new KeyboardEvent('keydown', { ctrlKey: true, key: 'h' });
+    window.dispatchEvent(event);
+
+    expect(alertMock).toHaveBeenCalledWith('Logging you out');
+    expect(logOutMock).toHaveBeenCalled();
+    
+    alertMock.mockRestore();
   });
 });
